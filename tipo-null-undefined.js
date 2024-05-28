4b4cfe041b5513fcbf51e26907704ebf1a066ad5
Diego Nonato <nonatodiego2014@gmail.com>
2024-05-27 21:38:49 -0300

    tipo null e undefined

---
+++ tipo-null-undefined.js
@@ -0,0 +1,12 @@
+let nameStudent;
+const teacher = 'Ana';
+console.log(nameStudent); // undefined
+console.log(typeof nameStudent); // undefined
+console.log(typeof teacher); // string
+
+let telephoneStudent = null;
+
+console.log(telephoneStudent + 3);
+console.log(nameStudent + 3);
+
+console.log(typeof telephoneStudent); // object
