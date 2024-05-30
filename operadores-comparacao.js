8f18d59dc3d8a07e2cbafe28b23e5fd05bd95e70
Diego Nonato <nonatodiego2014@gmail.com>
2024-05-30 17:07:12 -0300

    operadores de comparacao

---
+++ operadores-comparacao.js
@@ -0,0 +1,16 @@
+// Declaração de uma variável constante chamada 'estaAprovado' e atribuição do valor 'true'
+const estaAprovado = true;
+
+// Verificando se 'estaAprovado' é estritamente igual a 'true'
+if (estaAprovado === true) {
+    console.log('Aprovado!'); // Se 'estaAprovado' for verdadeiro, imprime 'Aprovado!'
+} 
+
+// Verificando se a string '0' é estritamente igual ao número 0
+if('0' === 0){
+    console.log('Igual'); // Se a string '0' for igual ao número 0, imprime 'Igual'
+} else {
+    console.log('Diferente'); // Se a string '0' não for igual ao número 0, imprime 'Diferente'
+}
+
+
